9c8cb18b50eb863f6e62f40f38fb6bd002ef7e76
Andrew Dodson <andrew.j.dodson@gmail.com>
2014-07-26 10:26:58 +1000

    Test for SSL certificate before opening SSL port

--- index.js
+++ index.js
@@ -12,30 +12,40 @@ if(process.env.NODE_ENV==='production'){
 	var app = server.listen( http_port );
 	// Start the peer server and bind listener
 	peer(app);
+
+	// Notify all has started
+	console.log("Peer.js HTTP server on port "+http_port);
+	console.log("Peer.js HTTPS server on port "+https_port);
+
 }
 else{
 	// For localhost
 	// HTTP
 	var http = require('http');
 	var app = http.createServer(server).listen(http_port);
-	console.log("HTTP server on port "+http_port);
 	// Start the peer server and bind listener
 	peer(app);
+	console.log("HTTP server on port "+http_port);
 
 	// HTTPS
-	var https = require('https');
-	var fs = require('fs');
-	var options = {
-		key : fs.readFileSync('../localhost.key').toString(),
-		cert : fs.readFileSync('../localhost.cert').toString()
-	};
-	var app = https.createServer(options, server).listen( https_port );
-	// Start the peer server and bind listener
-	peer(app);
-	console.log("HTTPS server on port "+ https_port );
+	// Do the .key and cert files exist?
+	var fs = require('fs'),
+		SSL_KEY = '../localhost.key',
+		SSL_CERT = '../localhost.cert';
+
+	fs.exists( SSL_KEY, function(bool){
+
+		if(!bool)
+			return;
+
+		var https = require('https');
+		var options = {
+			key : fs.readFileSync(SSL_KEY).toString(),
+			cert : fs.readFileSync(SSL_CERT).toString()
+		};
+		var app = https.createServer(options, server).listen( https_port );
+		// Start the peer server and bind listener
+		peer(app);
+		console.log("HTTPS server on port "+ https_port );
+	});
 }
-
-
-// Notify all has started
-console.log("Peer.js HTTP server on port "+http_port);
-console.log("Peer.js HTTPS server on port "+https_port);
